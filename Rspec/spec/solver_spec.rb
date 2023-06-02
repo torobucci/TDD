a8868339834b0eeb83d77b8adb9ed00cd0a84009
Peter Okorafor <peterdtitan@gmail.com>
2023-06-02 11:38:40 +0300

    Add tests for fizzbuzz method

--- Rspec/spec/solver_spec.rb
+++ Rspec/spec/solver_spec.rb
@@ -28,4 +28,23 @@ describe Solver do
       expect(solver.reverse('hello')).to eq('olleh')
     end
   end
+
+  describe 'fizzbuzz' do
+    it 'returns "fizzbuzz" when the number is divisible by both 3 and 5' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
+    end
+    it 'returns "fizz" when the number is divisible by 3' do
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+    it 'returns "buzz" when the number is divisible by 5' do
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+    it 'returns the number as a string for any other case' do
+      expect(solver.fizzbuzz(7)).to eq('7')
+      expect(solver.fizzbuzz(17)).to eq('17')
+    end
+  end
 end
